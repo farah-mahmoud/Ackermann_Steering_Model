fedc5ab747f7bdabaebf6cc7cd46ddcdca3601df
Farah Mahmoud <farahmahmoud031@gmail.com>
2025-05-03 01:37:09 +0300

    some updates

--- ws_farah/build/rqt_controller_manager/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/farah/ws_farah/install/rqt_controller_manager'
--- ws_farah/build/rqt_joint_trajectory_controller/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/farah/ws_farah/install/controller_manager_msgs/share/controller_manager_msgs/package.sh"
